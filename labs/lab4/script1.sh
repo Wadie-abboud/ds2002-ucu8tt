6e98031a249f0544df678d0fa34b05bc9629c56e
Wadie <ucu8tt@virginia.edu>
2025-02-28 18:10:01 -0500

    fixed small issue with script1

---
+++ labs/lab4/script1.sh
@@ -0,0 +1,8 @@
+LOCAL_FILE="$1"
+BUCKET_NAME="$2"
+EXPIRATION="$3"
+
+FILE_NAME=$(basename "$LOCAL_FILE")
+aws s3 cp "$LOCAL_FILE" "s3://$BUCKET_NAME/$FILE_NAME"
+PRESIGNED_URL=$(aws s3 presign --expires-in "$EXPIRATION" "s3://$BUCKET_NAME/$FILE_NAME")
+echo "$PRESIGNED_URL"
